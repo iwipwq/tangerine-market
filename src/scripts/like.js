8ecc499bd287a3a3f048f699ee1265e207bdfcbd
stoneboat <tood2111@gmail.com>
2022-01-11 00:23:45 +0900

    add like.js

---
+++ src/scripts/like.js
@@ -0,0 +1,18 @@
+const likeBtn = document.querySelector(".like");
+const iconHeart = likeBtn.querySelector(".icon-heart");
+const likeCounter = document.querySelector(".like-counter");
+
+//좋아요 기능
+function likeUpDown() {
+  if (iconHeart.classList.contains("on")) {
+    iconHeart.classList.remove("on");
+    iconHeart.src = "../../img/icon-heart.svg";
+    likeCounter.innerText = parseInt(likeCounter.innerText) - 1;
+  } else {
+    iconHeart.classList.add("on");
+    iconHeart.src = "../../img/icon-heart-fill.svg";
+    likeCounter.innerText = parseInt(likeCounter.innerText) + 1;
+  }
+}
+
+likeBtn.addEventListener("click", likeUpDown);
