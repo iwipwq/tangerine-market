61e16a59424915111f48c5a3e9d2c118369e3840
stoneboat <tood2111@gmail.com>
2022-01-11 17:44:07 +0900

    update

--- src/scripts/like.js
+++ src/scripts/like.js
@@ -2,7 +2,7 @@ const likeBtn = document.querySelector(".like");
 const iconHeart = likeBtn.querySelector(".icon-heart");
 const likeCounter = document.querySelector(".like-counter");
 
-//좋아요 기능
+//좋아요 기능 ui만
 function likeUpDown() {
   if (iconHeart.classList.contains("on")) {
     iconHeart.classList.remove("on");
